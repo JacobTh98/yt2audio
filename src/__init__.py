5748fce7251bb3b29cf3f40b85bcee5fc20b5322
JacobTh98 <jacob.thoenes@googlemail.com>
2023-03-07 11:35:15 +0100

    implemented title extraction

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,3 @@
-from .data_handling import read_conv_conf, overwrite_conv_conf
+from .data_handling import read_conv_conf, overwrite_conv_conf, get_title_from_url
 
-__all__ = [
-    "read_conv_conf",
-    "overwrite_conv_conf",
-]
+__all__ = ["read_conv_conf", "overwrite_conv_conf", "get_title_from_url"]
--- src/data_handling.py
+++ src/data_handling.py
@@ -1,4 +1,5 @@
 import json
+import urllib.request
 
 
 def read_conv_conf(conf_path: str = "conv_conf.json") -> dict:
@@ -30,3 +31,50 @@ def overwrite_conv_conf(new_config: dict) -> None:
     with open("conv_conf.json", "w") as fp:
         json.dump(new_config, fp)
     print(f"Config is changed to: \n {new_config}")
+
+
+def remove_word(title: str) -> str:
+    """
+    deletes the given words from "delete_words.txt" inside a string.
+
+    Parameters
+    ----------
+    title : str
+        string that has to be checked
+
+    Returns
+    -------
+    str
+        cheared string
+    """
+    f = open("delete_words.txt", "r")
+    del_wrds = f.read().split("\n")
+    for wrds in del_wrds:
+        title = title.replace(wrds, "")
+    return title
+
+
+def get_title_from_url(url: str, substitute_str:bool=True) -> str:
+    """
+    Extract the video title from a YouTube url.
+
+    Parameters
+    ----------
+    url : str
+        video url
+    substitute_str : bool, optional
+        enable or diable word substitution, by default True
+
+    Returns
+    -------
+    str
+        title, by default cleared string
+    """
+    with urllib.request.urlopen(url) as url:
+        title = url.read().decode("utf-8")
+        title = title.split("<title>")[1]
+        title = title.split("- YouTube</title>")[0]
+        print(title)
+        if substitute_str:
+            title = remove_word(title)
+    return title
